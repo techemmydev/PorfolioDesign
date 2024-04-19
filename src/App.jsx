964bc4eb9a937ccd5b3dde415a0652d1f27bfa45
kesh <efikunmi@yahoo.com>
2024-04-19 14:41:24 +0100

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import About from "./components/About/About";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div>
         <Navbar />
+        <About/>
       </div>
     </>
   );
