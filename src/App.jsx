2de8f0c3629b3d529e8600f2d72d52ccd29ced73
unknown <beejayolawumi2020@gmail.com>
2024-04-19 15:36:40 +0100

    service section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Service from "./components/service/Service";
-
-function App() {
-  return (
-    <>
-      <div>
-        <Navbar />
-        <Service />
-      </div>
-    </>
-  );
-}
-
-export default App;
-import "./App.css";
 import Aboutpage from "./components/Aboutpage/Aboutpage";
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
@@ -24,6 +9,7 @@ function App() {
       <div>
         <Navbar />
         <Aboutpage />
+        <Service />
       </div>
     </>
   );
